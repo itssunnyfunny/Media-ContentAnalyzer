92e89f089f691e38de57985243f98fc4c8f7a6ce
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-09-08 14:25:48 +0530

    adding a package : tailwind-config for all the tailwind configarations

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          brand: '#123456',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
